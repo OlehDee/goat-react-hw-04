5258fdc184c190e99478097eb302c4f21d3e56cb
Oleh_Denysenko <Oleh.Denysenkoo@gmail.com>
2025-05-16 09:00:40 +0300

    correct SearchBAr

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -2,38 +2,35 @@ import toast, { Toaster } from "react-hot-toast";
 import css from "./SearchBar.module.css";
 import { FiSearch } from "react-icons/fi";
 
-
 export default function SearchBar({ onFind }) {
   function submitHandler(event) {
     event.preventDefault();
-    const data = event.target.elements.search.value;
+    const data = event.target.elements.search.value.trim();
     if (data.length === 0) {
       toast.error("The field is empty!");
       return;
     }
-    onFind(event.target.elements.search.value);
+    onFind(data);
     event.target.reset();
   }
 
   return (
     <header className={css.header}>
-        <form onSubmit={submitHandler} className="search-form">
-  
-    <FiSearch className="search-icon" />
-    <input
-      type="text"
-      name="search"
-      placeholder="Search"
-      autoComplete="off"
-      autoFocus
-    />
-  
-</form>
+      <form onSubmit={submitHandler} className={css.searchForm}>
+        <FiSearch className={css.searchIcon} />
+        <input
+          type="text"
+          name="search"
+          placeholder="Search"
+          autoComplete="off"
+          autoFocus
+        />
+      </form>
       <Toaster
         position="top-center"
         containerClassName={css.toast}
         toastOptions={{ style: { color: "white", backgroundColor: "deepblue" } }}
-      > Щось пішло не так</Toaster>
+      />
     </header>
   );
 }
